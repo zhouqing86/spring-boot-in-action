2efdf719fa0bc51686807616bb4d16a567ec62e3
Qing Zhou <qing.zhou@rea-group.com>
2016-12-25 08:29:42 +0800

    Add test for spring boot

--- src/test/java/com/wendll/InactionApplicationTests.java
+++ src/test/java/com/wendll/InactionApplicationTests.java
@@ -1,16 +1,24 @@
 package com.wendll;
 
+import com.wendll.controller.BookController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InactionApplicationTests {
 
+	@Autowired
+	private BookController bookController;
+
 	@Test
-	public void contextLoads() {
+	public void contexLoads() throws Exception {
+		assertThat(bookController).isNotNull();
 	}
 
 }
