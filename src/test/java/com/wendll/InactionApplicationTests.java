ce16374ecdd23a051e1894803387a920a0a95f5b
Qing Zhou <qing.zhou@rea-group.com>
2016-12-25 10:25:34 +0800

    Add selenium test

--- src/test/java/com/wendll/InactionApplicationTests.java
+++ src/test/java/com/wendll/InactionApplicationTests.java
@@ -1,24 +1,64 @@
 package com.wendll;
 
-import com.wendll.controller.BookController;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+
+import com.wendll.InactionApplication;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = InactionApplication.class)
 public class InactionApplicationTests {
 
-	@Autowired
-	private BookController bookController;
+	@LocalServerPort
+	private int port;
 
-	@Test
-	public void contexLoads() throws Exception {
-		assertThat(bookController).isNotNull();
+	private static FirefoxDriver browser;
+
+	@BeforeClass
+	public static void openBrowser() {
+		browser = new FirefoxDriver();
+		browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
+	@AfterClass
+	public static void closeBrowser() {
+		browser.quit();
+	}
+
+	@Test
+	public void testHello() throws Exception {
+		String baseUrl = "http://localhost:" + port;
+		browser.get(baseUrl);
+		assertTrue(browser.getPageSource().contains("Hello World"));
+	}
 }
+
