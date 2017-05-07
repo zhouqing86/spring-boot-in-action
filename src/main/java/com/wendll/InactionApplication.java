6bce2fa3a7d2b0a4edafa644e964372c54c66669
Qing Zhou <qing.zhou@rea-group.com>
2017-05-07 23:20:19 +0900

    Try to make built war deployable to tomcat

--- src/main/java/com/wendll/InactionApplication.java
+++ src/main/java/com/wendll/InactionApplication.java
@@ -2,9 +2,16 @@ package com.wendll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InactionApplication {
+public class InactionApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(InactionApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(InactionApplication.class, args);
